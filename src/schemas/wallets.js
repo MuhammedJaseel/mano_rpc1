3bdf3072842fe70feb2d58b1162fd5706335bc1b
MuhammedJaseel <jaseelmanamulli@gmail.com>
2025-12-21 04:47:00 +0400

    TC

--- src/schemas/wallets.js
+++ src/schemas/wallets.js
@@ -5,7 +5,7 @@ const { Schema, model } = mongoose;
 const walletsSchema = new Schema({
   a: { type: String, required: true, unique: true, index: true },
   b: { type: String, required: true, default: "0" },
-  n: { type: Number, required: true, default: -1 },
+  n: { type: Number, required: true, default: 0 },
   ca: { type: Date, required: true, default: Date.now }, // createdAt
 });
 
